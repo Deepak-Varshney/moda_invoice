3505d7f1a081d57597bbe702afe33e28c9c39055
Deepak Varshney <138565507+Deepak-Varshney@users.noreply.github.com>
2024-08-02 10:55:42 +0530

    Update middleware.ts
    Update middleware.ts to bypass the auth

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,28 @@
-// Protecting routes with next-auth
-// https://next-auth.js.org/configuration/nextjs#middleware
-// https://nextjs.org/docs/app/building-your-application/routing/middleware
+// // Protecting routes with next-auth
+// // https://next-auth.js.org/configuration/nextjs#middleware
+// // https://nextjs.org/docs/app/building-your-application/routing/middleware
+
+// import NextAuth from 'next-auth';
+// import authConfig from './auth.config';
+// import { NextResponse } from 'next/server';
+
+// const { auth } = NextAuth(authConfig);
+
+// export default auth((req) => {
+//   if (true) {
+//     // Bypass authentication during development
+//     return NextResponse.next();
+//   }
+
+//   if (!req.auth) {
+//     const url = req.url.replace(req.nextUrl.pathname, '/');
+//     return Response.redirect(url);
+//   }
+// });
+
+// export const config = { matcher: ['/dashboard/:path*'] };
+
+bypassing the auth
 
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
@@ -9,15 +31,10 @@ import { NextResponse } from 'next/server';
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  if (true) {
-    // Bypass authentication during development
-    return NextResponse.next();
-  }
-
-  if (!req.auth) {
-    const url = req.url.replace(req.nextUrl.pathname, '/');
-    return Response.redirect(url);
-  }
+  // Bypass authentication during development
+  return NextResponse.next();
 });
 
-export const config = { matcher: ['/dashboard/:path*'] };
+export const config = {
+  matcher: ['/dashboard/:path*'], // Adjust this pattern as needed for your routes
+};
