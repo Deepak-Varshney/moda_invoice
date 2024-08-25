7c4f88f65f1819fe35337dd8910622cd7da9dec5
Deepak Varshney <138565507+Deepak-Varshney@users.noreply.github.com>
2024-08-25 09:20:34 +0530

    Update useBarcodeScan.tsx

--- hooks/useBarcodeScan.tsx
+++ hooks/useBarcodeScan.tsx
@@ -1,40 +1,35 @@
 import { useEffect, useRef } from 'react';
 
-export const useBarcodeScan = (onScan: (s: string) => void) => {
+export const useBarcodeScan = (
+  onScan: (s: string) => void,
+  resetTime: number = 100
+) => {
   const scannedCode = useRef<{ key: string; timeStamp: number }[]>([]);
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
+      const now = e.timeStamp;
+
       if (
         scannedCode.current.length > 0 &&
-        e.timeStamp - scannedCode.current.slice(-1)[0].timeStamp > 10
+        now - scannedCode.current.slice(-1)[0].timeStamp > resetTime
       ) {
         scannedCode.current = [];
       }
 
       if (e.key === 'Enter' && scannedCode.current.length > 0) {
-        let scannedString = scannedCode.current.reduce(function (
-          scannedString,
-          entry
-        ) {
-          return scannedString + entry.key;
-        }, '');
+        const scannedString = scannedCode.current
+          .map((entry) => entry.key)
+          .join('');
         scannedCode.current = [];
         onScan(scannedString);
-      }
-
-      if (e.key !== 'Shift') {
-        let data = JSON.parse(JSON.stringify(e, ['key', 'timeStamp']));
-        data.timeStampDiff =
-          scannedCode.current.length > 0
-            ? data.timeStamp - scannedCode.current.slice(-1)[0].timeStamp
-            : 0;
-        scannedCode.current.push(data);
+      } else if (e.key.length === 1 && e.key !== 'Shift') {
+        scannedCode.current.push({ key: e.key, timeStamp: now });
       }
     };
 
     document.addEventListener('keydown', handler);
 
     return () => document.removeEventListener('keydown', handler);
-  }, [onScan]);
+  }, [onScan, resetTime]);
 };
